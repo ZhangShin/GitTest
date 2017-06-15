c72c1a031c9d015030e5f95ae757458e8df729b9
zx <1174983038@qq.com>
2017-06-15 16:03:18 +0800

    add4

--- app/src/main/java/com/vogtec/gittest/MainActivity.java
+++ app/src/main/java/com/vogtec/gittest/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //add 1
+        //add4
     }
 }
