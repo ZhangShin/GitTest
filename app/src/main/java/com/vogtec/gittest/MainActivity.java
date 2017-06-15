f84dbd37b796ae14ebf8bc8bb3170f3bab0c43ca
zx <1174983038@qq.com>
2017-06-15 12:24:31 +0800

    reset

--- app/src/main/java/com/vogtec/gittest/MainActivity.java
+++ app/src/main/java/com/vogtec/gittest/MainActivity.java
@@ -9,7 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //add feature 1 on master
-        //add 2
     }
 }
