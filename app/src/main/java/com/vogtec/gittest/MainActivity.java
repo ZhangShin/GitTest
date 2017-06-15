f785186fd41a018d68820da58536782511d5ddc4
zx <1174983038@qq.com>
2017-06-15 12:20:44 +0800

    add 2

--- app/src/main/java/com/vogtec/gittest/MainActivity.java
+++ app/src/main/java/com/vogtec/gittest/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //add feature 1 on master
+        //add 2
     }
 }
