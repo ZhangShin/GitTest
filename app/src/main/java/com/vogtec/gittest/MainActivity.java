5bc4c4289aee112e7808020a293aa2c165b1535d
zx <1174983038@qq.com>
2017-06-15 14:47:06 +0800

    reset

--- app/src/main/java/com/vogtec/gittest/MainActivity.java
+++ app/src/main/java/com/vogtec/gittest/MainActivity.java
@@ -9,8 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //add 1
-        //add 2
-        //add 3
     }
 }
