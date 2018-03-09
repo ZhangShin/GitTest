310d1567bc1d830a01283b60520e5e2b462c8702
zhangxin <zhangxin@kokobao.com>
2018-03-09 10:02:18 +0800

    master change

--- app/src/main/java/com/vogtec/gittest/MainActivity.java
+++ app/src/main/java/com/vogtec/gittest/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Dev1
+        //Default
+        //master changes
     }
 }
